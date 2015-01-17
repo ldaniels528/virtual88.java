455fa07f24d433ab13499d45db9d4cae973d8b5e
Lawrence Daniels <lawrence.daniels@gmail.com>
2015-01-17 06:17:13 -0800

    updated test to provide the user with more information

--- src/test/java/org/ldaniels528/javapc/ibmpc/devices/cpu/opcodes/math/ADDTest.java
+++ src/test/java/org/ldaniels528/javapc/ibmpc/devices/cpu/opcodes/math/ADDTest.java
@@ -1,6 +1,7 @@
 package org.ldaniels528.javapc.ibmpc.devices.cpu.opcodes.math;
 
 import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Test;
 import org.ldaniels528.javapc.ibmpc.devices.cpu.I8086;
 import org.ldaniels528.javapc.ibmpc.devices.cpu.OpCode;
@@ -9,6 +10,8 @@ import org.ldaniels528.javapc.ibmpc.devices.display.IbmPcDisplayFrame;
 import org.ldaniels528.javapc.ibmpc.exceptions.X86AssemblyException;
 import org.ldaniels528.javapc.ibmpc.system.IbmPcSystem;
 import org.ldaniels528.javapc.ibmpc.system.IbmPcSystemFactory;
+
+import static java.lang.String.format;
 
 /**
  * <pre>
@@ -32,12 +35,20 @@ public class ADDTest {
         final IbmPcSystem system = IbmPcSystemFactory.getIBMPCjr(frame);
         final I8086 cpu = system.getCPU();
 
+        logger.info("Given AX = FFFEh");
         cpu.AX.set(0xFFFE);
+
+        logger.info("When AX is increased by +22 (signed byte)");
         final OpCode opCode = new ADD(cpu.AX, new SignedByteValue(22));
+        logger.info(format("\topCode: %s", opCode));
         cpu.execute(system, opCode);
 
+        logger.info(format("Then AX should equal 0014h"));
         logger.info(cpu.toString());
+        Assert.assertEquals(cpu.AX.get(), 0x0014);
 
+        logger.info("And the Carry flag should be set");
+        Assert.assertTrue(cpu.FLAGS.isCF());
     }
 
 }
